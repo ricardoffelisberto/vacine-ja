0ccfc07d914ced55f84bd5df4d94db01c94ce8d3
Ricardo Felisberto <ricardoffelisberto@gmail.com>
2022-04-12 15:03:24 -0300

    adicionar + lista vacinas

--- src/app/vacinas/vacinas.page.ts
+++ src/app/vacinas/vacinas.page.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-vacinas',
   templateUrl: './vacinas.page.html',
   styleUrls: ['./vacinas.page.scss'],
 })
-export class VacinasPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class VacinasPage {
+  private vacinas: Array<Object> = [];
+  constructor(public navCtrl: NavController) {
+    this.vacinas = [
+      {
+        "nome": "teste",
+      },
+      {
+        "nome": "teste2",
+      }
+    ]
   }
-
 }
